23d2cf12f43a9c36c960a15144c39a04b09c3736
Rhys <36913739+ryceg@users.noreply.github.com>
2022-03-10 18:20:25 +1100

    transition

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,39 +1,43 @@
 <script lang="ts">
 	import { Popover, PopoverButton, PopoverPanel } from '@rgossiaux/svelte-headlessui';
 	import { createPopperActions } from 'svelte-popperjs';
-
+	import { slide } from 'svelte/transition';
 	const [popperRef, popperContent] = createPopperActions();
-
 	// Example Popper configuration
 	const popperOptions = {
 		placement: 'bottom-end',
 		strategy: 'fixed',
 		modifiers: [{ name: 'offset', options: { offset: [0, 10] } }]
 	};
+	let open;
 </script>
 
-<header class="text-gray-600 body-font">
-	<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-		<a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0" href="./">
+<header class="body-font text-gray-600">
+	<div class="md:flex-row container flex flex-col flex-wrap items-center p-5 mx-auto">
+		<a class="title-font md:mb-0 flex items-center mb-4 font-medium text-gray-900" href="./">
 			<img src="static/PFS PNG Small.png" alt="PFS Logo" class="w-10" />
 			<span class="ml-3 text-xl">Pi Phi Sigma</span>
 		</a>
-		<nav class="md:ml-auto flex flex-wrap items-center justify-center text-sm sm:text-base">
+		<nav class="md:ml-auto sm:text-base flex flex-wrap items-center justify-center text-sm">
 			<Popover>
-				<PopoverButton use={[popperRef]}>Solutions</PopoverButton>
-				<PopoverPanel use={[[popperContent, popperOptions]]}>
-					<a class="mr-5 hover:text-gray-900" href="/about" sveltekit:prefetch>About</a>
-					<a class="mr-5 hover:text-gray-900" href="/chapters" sveltekit:prefetch>Chapters</a>
-					<a class="mr-5 hover:text-gray-900" href="/philanthropy" sveltekit:prefetch
-						>Philanthropy</a
-					>
-					<a class="mr-5 hover:text-gray-900" href="/resources" sveltekit:prefetch>Resources</a>
-				</PopoverPanel>
+				<PopoverButton use={[popperRef]} let:open>Solutions</PopoverButton>
+				{#if open}
+					<div transition:slide>
+						<PopoverPanel use={[[popperContent, popperOptions]]}>
+							<a class="hover:text-gray-900 mr-5" href="/about" sveltekit:prefetch>About</a>
+							<a class="hover:text-gray-900 mr-5" href="/chapters" sveltekit:prefetch>Chapters</a>
+							<a class="hover:text-gray-900 mr-5" href="/philanthropy" sveltekit:prefetch
+								>Philanthropy</a
+							>
+							<a class="hover:text-gray-900 mr-5" href="/resources" sveltekit:prefetch>Resources</a>
+						</PopoverPanel>
+					</div>
+				{/if}
 			</Popover>
 		</nav>
 		<button
 			href="./membership"
-			class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+			class="focus:outline-none hover:bg-gray-200 md:mt-0 inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded"
 			>Join Now
 			<svg
 				fill="none"
